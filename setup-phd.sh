06a7f4834f0940d3394d0607c8b0a029e62cc90a
root <root@ip-10-0-76-157.ap-southeast-2.compute.internal>
2016-04-23 12:42:12 +0000

    removed redundant file.

--- setup-phd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-useradd daemon-user
-mkdir /home/daemon-user
-chown daemon-user /home/daemon-user
-chown daemon-user /mnt/xvdb/ -R
-sudo -iu daemon-user /usr/local/phabricator/bin/phd start
